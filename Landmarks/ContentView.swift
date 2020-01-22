bcc7f88d30aa271502376fb96aba17c376fe0ac7
reycritt <reycrittenden@gmail.com>
2020-01-22 14:59:48 -0600

    Update to Landmarks

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Socstudent on 1/22/20.
+//  Copyright © 2020 Socstudent. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
